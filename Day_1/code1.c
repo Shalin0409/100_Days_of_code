c2d664866392cc417e83f2d6cdf5518ade7823cf
shalin0409 <Shalin0409@gmail.com>
2025-09-01 22:42:10 +0530

    code1

---
+++ Day_1/code1.c
@@ -0,0 +1,19 @@
+
+// Write a program to input two numbers and display their sum
+
+#include<stdio.h>
+int main() {
+    int a;
+    int b;
+    int c;
+    printf("enter a cdnumber\n");
+    printf("enter another number\n");
+    scanf("%d",& a);
+    scanf("%d",& b);
+    c=a+b;
+    printf("sum%d",c);
+    return 0;
+
+}
+
+
